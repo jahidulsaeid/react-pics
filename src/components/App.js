954befb1d5946dbba25161beaed65df065be663e
jahidulsaeid <jahidul.saeid@gmail.com>
2021-12-06 03:05:59 +0600

    update first loading

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
 import unsplash from "../api/unsplash";
 import ImageList from "./ImageList";
@@ -14,25 +14,23 @@ const App = () => {
     });
     if (response.status === 200) {
       setImages(response.data.results);
-      console.log(loading);
       setLoading(false)
     } else {
-      console.log("ERROR");
-      console.log(loading);
       setLoading(false)
     }
   }
+  useEffect(() => {
+    onSearchSubmit("cat")
+  }, [])
   return (
     <>
       {loading ? <Loading /> :
         <div className="ui container" style={{ marginTop: "20px" }}>
-          {console.log(images.length)}
           <SearchBar onSubmit={onSearchSubmit} />
-       
-          {images.length === 0 ? <h1 style={{textAlign:"center"}}>Sorry didn't find any image...</h1> :
-             <ImageList images={images} />
+          {images.length === 0 ?
+            <h1 style={{ textAlign: "center" }}>Sorry didn't find any image...</h1>:
+            <ImageList images={images} />
           }
-          
         </div>
 
       }
